b0cc564306f8be58429c6d209972d16b6ac4e128
Calvin <calvinkioko91@gmail.com>
2022-06-05 01:10:41 +0300

    commit tree 02d0898a4afb01493d7f38d99d269d8453d2c6ab

---
+++ index.js
@@ -0,0 +1,35 @@
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    // todo
+    return string.toLowerCase();
+  }
+  function logShout(string) {
+    // todo
+    console.log(string.toUpperCase());
+  }
+
+  function logWhisper(string) {
+    // todo
+    console.log(string.toLowerCase());
+  }
+  function sayHiToHeadphonedRoommate(string) {
+    // todo
+    var a = "I can't hear you!";
+    var b = "YES INDEED!";
+    var c = "I would love to!";
+    if(string.toLowerCase(string)==string){
+       return a;
+    }
+
+   else if(string.toUpperCase(string) == string){
+    return b;
+   }
+   else if("Let's have dinner together!"==string){
+       return c;
+   }
+
+   
+  }
